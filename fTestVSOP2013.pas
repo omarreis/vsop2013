086dc06ff6bae4099665c1bb5d465d8a6f667ac9
Omar Reis <omarreis@hotmail.com>
2020-07-17 09:28:10 -0300

    1st version

---
+++ fTestVSOP2013.pas
@@ -0,0 +1,301 @@
+unit fTestVSOP2013;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.Math.Vectors,
+
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.Objects,
+  vsop2013, FMX.Edit;
+
+type
+  TForm2 = class(TForm)
+    Memo1: TMemo;
+    btnLoadFile: TButton;
+    labPercent: TLabel;
+    pbChart: TPaintBox;
+    TimerAnimatePlanets: TTimer;
+    cbAnimatePlanets: TSwitch;
+    Label1: TLabel;
+    labTime: TLabel;
+    tbScale: TTrackBar;
+    labScale: TLabel;
+    tbAnimationSpeed: TTrackBar;
+    labAnimationSpeed: TLabel;
+    edFilename: TEdit;
+    btnTests: TButton;
+    btnCalc: TButton;
+    edPlanet: TEdit;
+    Label2: TLabel;
+    Labw: TLabel;
+    edJDE: TEdit;
+    procedure btnLoadFileClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure cbAnimatePlanetsSwitch(Sender: TObject);
+    procedure TimerAnimatePlanetsTimer(Sender: TObject);
+    procedure pbChartPaint(Sender: TObject; Canvas: TCanvas);
+    procedure tbScaleChange(Sender: TObject);
+    procedure tbAnimationSpeedChange(Sender: TObject);
+    procedure btnTestsClick(Sender: TObject);
+    procedure btnCalcClick(Sender: TObject);
+  private
+    procedure Form2LoadPropgress(Sender: TObject; aPerc: integer);
+    procedure showVectors(ip: integer; const jde: Double; const Position,
+      Speed: TVector3D);
+  public
+    fVSOPFile:T_VSOP2013_File;
+    fBitmap:TBitmap;             // Sky chart
+    fPosPlanets:Array[1..NUM_PLANETS] of TVector3D;
+
+    fanimJDE:double;
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm2.FormCreate(Sender: TObject);
+var i:integer;
+begin
+  fVSOPFile := nil;
+  for i:=1 to NUM_PLANETS do fPosPlanets[i] := Vector3D(0,0,0);
+
+  fBitmap := TBitmap.Create;
+end;
+
+const
+  PLANET_COLORS:Array[1..NUM_PLANETS] of TAlphaColor=
+    ( TAlphaColorRec.Red,          // 1 Mercury
+      TAlphaColorRec.Lightsalmon,  // 2 Venus
+      TAlphaColorRec.Aqua,         // 3 Earth
+      TAlphaColorRec.Red,          // 4 Mars
+      TAlphaColorRec.White,        // 5 Jupiter
+      TAlphaColorRec.Green,        // 6 Saturn
+      TAlphaColorRec.Aliceblue,    // 7 Uranus
+      TAlphaColorRec.Azure,        // 8 Neptune
+      TAlphaColorRec.Bisque        // 9 Pluto
+    );
+
+  PLANET_RADIUS:Array[1..NUM_PLANETS] of integer=  //in pixels
+    ( 2,     // 1 Mercury
+      3,     // 2 Venus
+      3,     // 3 Earth
+      3,     // 4 Mars
+      5,     // 5 Jupiter
+      5,     // 6 Saturn
+      4,     // 7 Uranus
+      4,     // 8 Neptune
+      1      // 9 Pluto
+    );
+
+
+procedure TForm2.pbChartPaint(Sender: TObject; Canvas: TCanvas);
+var ip:integer; aPos:TVector3D; aScP:TVector; aR:TRectF; C:TVector; aScale,aRadius:Double;
+begin
+  // paint fBitmap
+  C := Vector(300,300); //center of chart = Sun pos (heliocantric coordinates)
+
+  if (fBitmap.Width=0) then //first paint
+    begin
+      fBitmap.SetSize(600,600);
+      fBitmap.Canvas.BeginScene;
+      fBitmap.Canvas.Clear( TAlphacolorRec.Black );  //reset
+    end
+    else begin
+      fBitmap.Canvas.BeginScene;
+      aR := RectF(0,0,600,600);
+      fBitmap.Canvas.Fill.Color := $08000000;   // draw transparent dark rect to darken the past gradually
+      fBitmap.Canvas.FillRect(aR,0,0,AllCorners,1.0);
+     end;
+
+  //draw Sun at the center
+  aR := RectF(C.x-6, C.y-6, C.x+6, C.y+6);
+  // fBitmap.Canvas.Stroke.Kind  := TBrushKind.Solid;
+  fBitmap.Canvas.Fill.Kind   := TBrushKind.Solid;
+  fBitmap.Canvas.Fill.Color  := TAlphaColorRec.Yellow;
+  fBitmap.Canvas.Stroke.Kind := TBrushKind.Solid;
+
+
+  fBitmap.Canvas.FillEllipse(aR, 1.0);
+
+  aScale := tbScale.Value;  //=pix/au
+
+  for ip := 1 to NUM_PLANETS do
+     begin
+       aPos := fPosPlanets[ip];
+       //convert to screen coordinates
+       aScP := Vector( C.x+aPos.x*aScale,C.Y+aPos.y*aScale);  //convert au-->pix
+
+       aRadius := PLANET_RADIUS[ip];
+
+       aR := RectF(aScP.X-aRadius, aScP.Y-aRadius, aScP.X+aRadius, aScP.Y+aRadius);
+       fBitmap.Canvas.Fill.Color := PLANET_COLORS[ip];
+       fBitmap.Canvas.FillEllipse(aR, 1.0);
+
+       if (ip=6) then //saturn rings ;)
+         begin
+           fBitmap.Canvas.Stroke.Color := PLANET_COLORS[ip];
+           fBitmap.Canvas.DrawLine( PointF(aScP.X-8,aScP.Y), PointF(aScP.X+8,aScP.Y), 1.0);;
+         end;
+     end;
+   fBitmap.Canvas.EndScene;
+
+   aR := RectF(0,0,600,600);           //copy to pb
+   Canvas.DrawBitmap(fBitmap,aR,aR,1.0);
+end;
+
+procedure TForm2.tbAnimationSpeedChange(Sender: TObject);
+begin
+  labAnimationSpeed.Text := Format('%4.0f',[tbAnimationSpeed.Value])+' days/tick (t=200ms)';
+end;
+
+procedure TForm2.tbScaleChange(Sender: TObject);
+begin
+  labScale.Text := IntToStr( Trunc(tbScale.Value) )+' pix/au';
+  if (fBitmap.Width<>0) then
+    begin
+      fBitmap.Canvas.BeginScene;
+      fBitmap.Canvas.Clear( TAlphacolorRec.Black );
+      fBitmap.Canvas.EndScene;
+    end;
+end;
+
+procedure TForm2.showVectors(ip:integer; const jde:Double; const Position,Speed:TVector3D);
+begin
+  Memo1.Lines.Add('');
+  Memo1.Lines.Add( PLANET_NAMES[ip] );
+  Memo1.Lines.Add('jde:'+Trim(Format('%8.1f',[ jde ])));
+  // pos
+  Memo1.Lines.Add('x: '+Trim(Format('%18.14f ua',[ Position.x ])));
+  Memo1.Lines.Add('y: '+Trim(Format('%18.14f ua',[ Position.y ])));
+  Memo1.Lines.Add('z: '+Trim(Format('%18.14f ua',[ Position.z ])));
+  // spd
+  Memo1.Lines.Add('sx: '+Trim(Format('%18.14f ua/d',[ Speed.x ])));
+  Memo1.Lines.Add('sy: '+Trim(Format('%18.14f ua/d',[ Speed.y ])));
+  Memo1.Lines.Add('sz: '+Trim(Format('%18.14f ua/d',[ Speed.z ])));
+end;
+
+
+procedure TForm2.btnTestsClick(Sender: TObject);
+var Position,Speed:TVector3D; ip:integer; jde:double;
+begin
+  // calculation tests extracted from VSOP2013_ctl.txt
+
+  // MERCURY     JD2405730.5  X :  0.242020971329 ua    Y : -0.352713705683 ua    Z : -0.051047411323 ua
+  //                            X':  0.017592067303 ua/d  Y':  0.017315449357 ua/d  Z': -0.000208715093 ua/d
+  ip  := 1;            //Mercury
+  jde := 2405730.5;
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+
+  //JUPITER     JD2816124.5  X : -0.280774899591 ua    Y :  5.124152830950 ua    Z : -0.018441408820 ua
+  //                         X': -0.007646343544 ua/d  Y': -0.000050903873 ua/d  Z':  0.000168106750 ua/d
+  ip  := 5;            //jupiter
+  jde := 2816124.5;
+
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+
+  // JUPITER     JD2405730.5  X : -5.392780445602 ua    Y : -0.805698954496 ua    Z :  0.124332318817 ua
+  //                          X':  0.001019284060 ua/d  Y': -0.007116469431 ua/d  Z':  0.000005921462 ua/d
+  ip  := 5;            //jupiter
+  jde := 2405730.5;
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+
+
+  // EARTH-MOON  JD2268932.5  X : -0.437662445161 ua    Y :  0.880925943295 ua    Z :  0.000970542639 ua
+  //                          X': -0.015692704507 ua/d  Y': -0.007712480753 ua/d  Z': -0.000010013711 ua/d
+  ip  := 3;            // Earth
+  jde := 2268932.5;
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+
+
+  // MARS        JD2542528.5  X : -1.501298042540 ua    Y :  0.720630252585 ua    Z :  0.051244837632 ua
+  //                          X': -0.005550687750 ua/d  Y': -0.011409922095 ua/d  Z': -0.000106501220 ua/d
+  ip  := 4;            //Mars
+  jde := 2542528.5;
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+
+
+  // SATURN      JD2542528.5  X : -7.484629691185 ua    Y : -6.379153813306 ua    Z :  0.409052899765 ua
+  //                          X':  0.003302297240 ua/d  Y': -0.004260528208 ua/d  Z': -0.000060046705 ua/d
+  ip  := 6;            //Saturn
+  jde := 2542528.5;
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+
+end;
+
+
+
+procedure TForm2.cbAnimatePlanetsSwitch(Sender: TObject);
+begin
+  TimerAnimatePlanets.Enabled := cbAnimatePlanets.IsChecked;
+  if TimerAnimatePlanets.Enabled then
+       fanimJDE := jd2000;    //start animation from jd2000
+end;
+
+// 200 ms = 5 ticks per second
+procedure TForm2.TimerAnimatePlanetsTimer(Sender: TObject);
+var ip:integer; aPosition,aSpeed:TVector3D; Year:Double;
+begin
+  if Assigned(fVSOPFile) then //upd
+    begin
+      Year := (fanimJDE-jd2000)/365.2422+2000.0;  // more or less :)
+      labTime.Text := Format('%6.2f',[Year]);
+      for ip := 1 to NUM_PLANETS do
+        begin
+          try
+            if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}fanimJDE, {out:} aPosition, aSpeed) then
+              fPosPlanets[ip] := aPosition;
+          except
+            TimerAnimatePlanets.Enabled := false;
+            raise Exception.Create('error in calculation: file range exceded');
+          end;
+        end;
+      fanimJDE := fanimJDE + tbAnimationSpeed.Value;  // + advance animation time
+
+      pbChart.Repaint;
+    end;
+
+end;
+
+// Loads vsop2013 ASCII file and performs a few calculations
+procedure TForm2.btnCalcClick(Sender: TObject);
+var Position,Speed:TVector3D; ip:integer; jde:double;
+begin
+  ip  := StrToInt(edPlanet.Text);
+  jde := StrToFloat(edJDE.Text);
+  if fVSOPFile.calculate_coordinates( {ip:}ip , {jde:}jde, {out:} Position, Speed) then
+   showVectors(ip,jde,Position,Speed) else Memo1.Lines.Add('error');
+end;
+
+procedure TForm2.btnLoadFileClick(Sender: TObject);
+var aFN:String;
+begin
+  fVSOPFile := T_VSOP2013_File.Create;
+  fVSOPFile.OnLoadProgress := Form2LoadPropgress;
+  // reads and parses long ASCII file: wait..
+  // te
+  aFN := Trim( edFilename.Text );    // '\dpr4\vsop2013\VSOP2013.p2000'  1500-3000. ( includes current time )
+  fVSOPFile.Read_ASCII_File( aFN );  // load test file
+
+  Memo1.Lines.Add(aFN+' Loaded');
+
+end;
+
+procedure TForm2.Form2LoadPropgress(Sender:TObject; aPerc:integer);
+begin
+  labPercent.Text := IntToStr(aPerc)+'%';
+  labPercent.Repaint;
+end;
+
+end.
---
+++ vsop2013.pas
@@ -0,0 +1,350 @@
+unit vsop2013;       // Delphi implementation of theory VSOP2013
+// This is Delphi port from original Fortran code by FRANCOU & SIMON
+// VSOP original files:
+//  ftp://ftp.imcce.fr/pub/ephem/planets/vsop2013/ephemerides/
+//  VSOP2013.m2000, VSOP2013.m1000, VSOP2013.p1000,VSOP2013.p2000,VSOP2013.p4000
+//  Files are large 400 MB ASCII containing chebyshev polynomial 1st kind coeficients.
+// There are 6 coefs/line ( 163 x )
+// The file header constains a table loc[,] of indexes into the coefs[] array
+// Each planet has a number of coefs.
+//
+// History:
+//   jun 2020: Loose port v0.9 from Fortran by oMAR
+//     this code loads coefs from ASCII file and builds memory tables (instead of original BIN files)
+//     note: Fortran language is '1 based' (array indexes all start with 1 (at least this code is) )
+
+interface
+
+uses
+  System.Math,           //NaN
+  System.Math.Vectors,
+  System.SysUtils;
+
+const
+  TXT_FILES_ROOT = './ephemerides/';
+  NUM_COEFS=978;   // fixed ?
+  NUM_FILES=6;     // one file for each 1500y period
+  NUM_PLANETS=9;
+  //
+  // NUM_PERIODS=17122;
+
+var
+   // original file names. Each of these files is 400 MB !
+   TXT_FILES : Array[1..NUM_FILES] of string = (
+             'VSOP2013.m4000',
+             'VSOP2013.m2000',
+             'VSOP2013.m1000',
+             'VSOP2013.p1000',
+             'VSOP2013.p2000',
+             'VSOP2013.p4000');
+
+  PLANET_NAMES :Array[1..NUM_PLANETS] of string =
+     //   1         2       3        4         5         6         7          8        9
+     ('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto');
+
+const
+  jd2000=2451545.0;
+  TestFileName='VSOP2013.p2000.bin';
+
+type
+   TDateRange=record  // epoch range covered by a given file
+     ti,tf:Double;
+   end;
+
+var
+   DATE_RANGES: Array[1..NUM_FILES] of TDateRange = (
+                  (ti:77294.5   ;tf: 625198.5),       // -4500 to -3000       VSOP2013.m4000
+                  (ti:625198.5  ;tf: 1173102.5),      // -3000 to -1500       VSOP2013.m2000
+                  (ti:1173102.5 ;tf: 1721006.5),      // -1500 to 0           VSOP2013.m1000
+                  (ti:1721006.5 ;tf: 2268910.5),      // 0 to +1500           VSOP2013.p1000
+                  (ti:2268910.5 ;tf: 2816814.5),      // +1500 to +3000       VSOP2013.p2000
+                  (ti:2816814.5 ;tf: 3364718.5));     // +3000 to +4500       VSOP2013.p4000
+type
+  // VSOP2013File loads a bin file and parses it into a Chebyshev Polynomial
+  // indexing and evaluation machine.
+
+  T_VSOP2013_LoadProgress=procedure( Sender:TObject; Percent:integer) of Object;
+
+  T_VSOP2013_Period=class  // a 32.0 day VSOP2013 period
+  private
+  public
+    dj1,dj2:Double;                       // bounds
+    coef:Array[1..NUM_COEFS] of Extended; // coefs
+    Constructor Create;
+    Destructor  Destroy; override;
+    procedure   Reset;
+  end;
+
+  T_VSOP2013_File=class
+  private
+    fOnLoadProgress:T_VSOP2013_LoadProgress;
+  public
+    //header
+    idf:integer;   // file identification.  Should read 2013
+    t1,t2:Double;  // file time range in JD
+    delta:Double;  // =32.0  number of days per period
+    nintv:integer; // =number of 32d periods ( formerly NUM_PERIODS )
+    ncoef:integer; // =0978 number of Chebyshev coeficients / period
+
+    loc:Array[1..3,1..9] of integer;     // indexes into coeficient arrays
+
+    Periods:Array of T_VSOP2013_Period;  // dynamic allocated Periods array
+
+    Constructor Create;
+    Destructor  Destroy; override;
+    Procedure   Reset;
+    function    Read_ASCII_File(const aFilename:String):boolean;  //read vsop2013 ASCII file to memory
+    function    calculate_coordinates(ip: integer;const jde:Double; var Position, Speed: TVector3D): boolean;
+
+    property    OnLoadProgress:T_VSOP2013_LoadProgress read fOnLoadProgress write fOnLoadProgress;
+  end;
+
+implementation
+
+{ T_VSOP2013_Period }
+
+constructor T_VSOP2013_Period.Create;
+begin
+  inherited Create;
+  Reset;
+end;
+
+destructor T_VSOP2013_Period.Destroy;
+begin
+  inherited;
+end;
+
+Procedure  T_VSOP2013_Period.Reset;  // zero vars
+var i:integer;
+begin
+  dj1:=0;
+  dj2:=0;
+  for i :=1 to NUM_COEFS do coef[i]:=0;
+end;
+
+{ T_VSOP2013_File }
+
+constructor T_VSOP2013_File.Create;
+begin
+  inherited Create;
+
+  Reset;
+  fOnLoadProgress:=nil;
+end;
+
+Procedure  T_VSOP2013_File.Reset;  // zero vars
+var i,j:integer;
+begin
+  idf   := 0;
+  t1    := 0;
+  t2    := 0;
+  delta := 0;
+  nintv := 0;  // num of 32d periods. =size of Periods[]
+  ncoef := 0;  // always 978 ??
+
+  for i:=1 to 3 do for j:= 1 to 6 do loc[i,j]:=0;
+
+  SetLength(Periods,0);  //clear previous, if any
+end;
+
+destructor T_VSOP2013_File.Destroy;
+var i:integer;
+begin
+  for i:=1 to nintv do Periods[i].Free;
+  SetLength(Periods,0);  //clear previous, if any
+
+  inherited;
+end;
+
+function T_VSOP2013_File.Read_ASCII_File(const aFilename:String):boolean;  //read vsop2013 ASCII file to memory
+var
+  InFile:Textfile;
+  S,ss:String;
+  i,j,k,l,ix,prevPerc,aPerc:integer;
+  aPeriod:T_VSOP2013_Period;
+  sMant,sExp:String;
+  aMant:Extended;
+  aExp:integer;
+
+begin
+  If FileExists(aFileName) then
+    begin
+      AssignFile(InFile,aFileName);
+      try
+        System.Reset(InFile);
+      except
+        raise Exception.Create('Error opening VSOP2013 file');
+      end;
+
+      prevPerc:=0;
+
+      try
+          // read vsop2013 file header
+          //  2013
+          //  2268910.5
+          //  2816814.5
+          //  32.0
+          //  017122
+          //  0978
+          Readln(InFile,S);   idf   := StrToIntDef(S,0);
+          Readln(InFile,S);   t1    := StrToFloatDef(S,0);
+          Readln(InFile,S);   t2    := StrToFloatDef(S,0);
+          Readln(InFile,S);   delta := StrToFloatDef(S,0);
+          Readln(InFile,S);   nintv := StrToIntDef(S,0);   //number of intervals in this file
+          Readln(InFile,S);   ncoef := StrToIntDef(S,0);
+
+          if (idf<>2013)    or  //sanity test in header
+             (nintv<>17122) or  //CHECK: are all these parameters fixed ??
+             (ncoef<>978) then
+              Raise Exception.Create('some error in file');
+
+          for i:=1 to 3 do  //read loc[,] table
+            begin
+              Readln(InFile,S); //load line
+              //        123456789.123456789.123456789.123456789.123456
+              // tipo: '  0001 0337 0469 0637 0715 0781 0841 0895 0937'
+              for j:=1 to NUM_PLANETS do
+                begin
+                  ss := Copy(S,2+5*(j-1), 5);   // there is one extra ' ' in the begining
+                  loc[i,j] := StrToInt( ss );   //
+                end;
+            end;
+
+          SetLength(Periods, nintv+1);          // alloc Periods[]
+
+          // Periods[0] not used
+
+          for i:=1 to nintv do  //  nintv = num of 32d periods ( =17122  1 based )
+            begin
+              aPeriod := T_VSOP2013_Period.Create;
+              Readln(InFile,S);    // read period's jd1,jd2
+              //       123456789.123456789.123
+              // tipo '  2268910.5  2268942.5'
+              ss := Copy(S,2,10);
+              aPeriod.dj1 := StrToFloat(ss);
+              ss := Copy(S,13,10);
+              aPeriod.dj2 := StrToFloat(ss);
+
+              for k := 1 to 163 do  // 163 lines of coefs
+                begin
+                  Readln(InFile,S);  //read coef line ( 6 coefs[] per line )
+                  //       123456789.123456789:123456789T
+                  // tipo '   -0.2536115476181505 +00  0.7009448388105931 -01  0.3144971753049469 -02 -0.1722448640420403 -03  0.8042649431438953 -06 -0.5011012033888969 -07'
+                  for L := 0 to 5 do
+                    begin
+                      sMant := Copy(S, 3+L*24,20);   //mantissa
+                      sExp  := Copy(S,23+L*24,4);   //exp
+                      aMant := StrToFloat(sMant);
+                      aExp  := StrToInt(sExp);
+                      ix    := (k-1)*6+L+1;
+                      aPeriod.coef[ ix ] := Power10( aMant,aExp );
+                    end;
+                end;
+
+              Periods[i] := aPeriod;  //save loaded Period in dynamic array
+
+              if Assigned(fOnLoadProgress) and (nintv<>0) then //load progress
+                begin
+                  aPerc := Trunc(i/nintv*100);
+                  if (aPerc<>prevPerc) then  //report once per %
+                    begin
+                      fOnLoadProgress(Self,aPerc);
+                      prevPerc := aPerc;
+                    end;
+                end;
+            end;
+          CloseFile(InFile);
+      except
+        CloseFile(InFile);  //close anyway
+        raise Exception.Create('Error loading file');
+      end;
+
+    end
+    else raise Exception.Create('File not found');
+end;
+
+function T_VSOP2013_File.calculate_coordinates( ip:integer; const jde:Double; {out:} var Position,Speed:TVector3D):boolean;
+//         Attempt to calculate the position and velocity for the specified planet.
+//         :param ip: index of planet to perform calculation for
+//         :param jde: the julian date to perform the calculation for
+//         :return: an array of the X, Y, and Z heliocentric position of the planet and the X', Y', and Z' heliocentric velocity
+//         ( units au and au/d )
+
+var i,j,ifile,iper,iad,ncf,nsi,ik,iloc,jt,jp:integer;
+    r1,rng:TDateRange;
+    aPeriod:T_VSOP2013_Period;
+    delta2,dj0:Double;
+    x:Extended;
+    tn:Array[1..20] of Extended; //Chebyshev factors
+    r :Array[1..6] of Extended;  //planet pos and speed  vectors
+
+begin
+  result := false;
+
+  // sanity check params
+  if (ip < 1) or (ip > NUM_PLANETS) then
+       raise Exception.Create('Invalid planet index');
+
+  if (jde<t1) or (jde>t2) then  // file bounds
+       raise Exception.Create('time out of range of the loaded file');
+
+  // rng.ti:=0; rng.tf:=0;
+  // ifile :=-1;
+  //
+  // for i:=1 to NUM_FILES do  //find the file
+  //   begin
+  //     r1 := DATE_RANGES[i];
+  //     if (jde>=r1.ti) and (jde<r1.tf) then
+  //       begin
+  //         rng  := r1;
+  //         ifile:=i;
+  //         break;
+  //       end;
+  //   end;
+
+  // if rng.ti=0 then
+  //       raise Exception.Create('Invalid jde');
+
+  iper := Trunc( (jde - t1) / delta)+1;  // calc 32d period index ( 1 based )
+  if (iper>0) and (iper<=nintv) then
+      begin
+        aPeriod := Periods[iper];  //get period containing jde
+
+        iad := loc[1,ip];   // initial term of the planet (index into coef[])
+        ncf := loc[2,ip];   // number of Chebyshev coefs
+        nsi := loc[3,ip];   // some delta divider ?
+
+        delta2 := delta/nsi; // default delta is 32.0, but it can be divided by nsi ??
+
+        ik := Trunc( (jde-aPeriod.dj1)/delta2 );
+        if (ik=nsi) then ik := ik-1;
+        iloc := iad + 6*ncf*ik;      // iloc = last term??
+        dj0  := aPeriod.dj1 + ik*delta2;
+
+        x    := 2.0*(jde-dj0)/delta2 - 1.0;  // Chebyshev time x in interval -1..+1
+        // build Chebyshev terms
+        tn[1] := 1.0;
+        tn[2] := x;
+        for i := 3 to ncf do
+          tn[i] := 2.0*x*tn[i-1]-tn[i-2];
+        // calculate planet pos & speed
+        for i:=1 to 6 do  // 6 vars ( pos and speed)
+          begin
+            r[i]:=0;            //zero sumatory
+            for j:=1 to ncf do  //loop ncf terms
+              begin
+                jp   := ncf-j+1;               //index into tn[], work backwards ??
+                jt   := iloc+ncf*(i-1)+jp-1;   //index into coef[]
+                if (jt<1) or (jt>nintv) then raise Exception.Create('invalid coef index'); //sanity check index
+                r[i] := r[i] + tn[jp]*aPeriod.coef[jt];
+              end;
+          end;
+        // copy vectors
+        Position := Vector3D(r[1],r[2],r[3]);
+        Speed    := Vector3D(r[4],r[5],r[6]);
+        result   := true;
+      end
+      else raise Exception.Create('period outside bounds');
+end;
+
+end.
