fd7719f8473b7d8cc149fa4c756cb3fe10f5dc6f
Omar Reis <omarreis@hotmail.com>
2023-04-05 11:08:09 -0300

    Add files via upload

--- TestVSOP2013.dpr
+++ TestVSOP2013.dpr
@@ -3,13 +3,13 @@ program TestVSOP2013;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  fTestVSOP2013 in 'fTestVSOP2013.pas' {Form2},
+  fTestVSOP2013 in 'fTestVSOP2013.pas' {FormVSOP2013Tests},
   vsop2013 in 'vsop2013.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TFormVSOP2013Tests, FormVSOP2013Tests);
   Application.Run;
 end.
